2dc28181a6b9982313e57bd3062923caef499f18
DESKTOP-LPMIHQM\Guilherme Cunha <1201506@isep.ipp.pt>
2022-12-04 22:43:27 +0000

    LP-18 #time 2h #comment Minimal change on the algorithm

--- ALGAV/us1.pl
+++ ALGAV/us1.pl
@@ -5,7 +5,7 @@
 
 todosTrajetorias(ListaP,Tempo,Data,Camiao):- findall(ID, entrega(_,Data,_,ID,_,_),Lista),
 					      permutation(Lista,ListaP),
-					      calcula_tempo(ListaP,Tempo,Data, Camiao).
+					      calcula_tempo(ListaP,Tempo,Data, Camiao);true.
                     	
 
 
